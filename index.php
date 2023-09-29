5365dbbf21776541778845b6b4359e1366e5b510
LEGENDARY1209 <130669909+LEGENDARY1209@users.noreply.github.com>
2023-09-29 08:51:33 -0300

    commit

--- index.php
+++ index.php
@@ -1,31 +1,22 @@
 <?php
     // Verificação: 5 é maior que 2?
-    $valor1 = 5;
-    $valor2 = 2;
-
-    if ($valor1 > $valor2) {
+    if (5 > 2) {
         echo "5 é maior que 2. <br>";
     } else {
         echo "5 não é maior que 2. <br>";
     }
 
     // Verificação: Robson é diferente de Pedro?
-    $nome1 = "Robson";
-    $nome2 = "Pedro";
-
-    if ($nome1 != $nome2) {
+    if ("Robson" != "Pedro") {
         echo "Robson é diferente de Pedro. <br>";
     } else {
         echo "Robson não é diferente de Pedro. <br>";
     }
 
     // Verificação: 12 é menor ou igual a 11?
-    $numero1 = 12;
-    $numero2 = 11;
-
-    if ($numero1 <= $numero2) {
+    if (12 <= 11) {
         echo "12 é menor ou igual a 11. <br>";
     } else {
-        echo "  12 não é menor ou igual a 11. <br>";
+        echo "12 não é menor ou igual a 11. <br>";
     }
 ?>
